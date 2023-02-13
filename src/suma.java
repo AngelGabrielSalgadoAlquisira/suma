5d4388f2ba1acc2b27acd0e1350bf8e015bc93fd
Angel Gabriel <alquisira2004@gmail.com>
2023-02-13 16:34:44 -0600

    creacion suma

---
+++ src/suma.java
@@ -0,0 +1,9 @@
+public class suma {
+    public static void main ( String[] args ) {
+        int a = 15, b = 10,  c = 10;
+        c = a + b;
+        System.out.println("La suma de a y b es: " + c);
+
+
+    }
+}
