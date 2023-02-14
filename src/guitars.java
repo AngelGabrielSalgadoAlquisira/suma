ce81a717ff50d7c5f64d9edbabc0e6335f2f7307
Angel Gabriel <alquisira2004@gmail.com>
2023-02-14 07:37:16 -0600

    se crea clase guitars

---
+++ src/guitars.java
@@ -0,0 +1,19 @@
+public class guitars {
+
+    private String serialNumber;
+
+    private double price;
+
+
+    private String builder;
+
+    private String model;
+
+
+    private String type;
+
+    private String backWood;
+
+    private String topWood;
+
+}
